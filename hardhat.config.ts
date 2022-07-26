37592a45f5672c12b825c81d34ede19de137308a
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-07-26 19:26:38 +0600

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,13 @@
+require ('@nomiclabs/hardhat-waffle')
+
+module.exports={
+  solidity: '0.8.2',
+  networks: {
+    rinkby: {
+      url: '',
+      accounts: [
+        '0009de15bb379eb113ba905ea8b051b761824ed58cb153ff49b2c9dba9dbf05b'
+      ]
+    }
+  }
+}
